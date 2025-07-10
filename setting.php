9f19f3cd8f223f764cb3934179ca575c969bcf79
kokopmeekk <ditzstoreoke@gmail.com>
2025-07-10 20:29:57 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,7 +1,7 @@
 <?php
 // Konfigurasi Panel
-$panel_url = 'https://';
+$panel_url = 'https://ditzhosting.panel-freefire.biz.id';
 // API Keys
-$plta_key = 'ptla_'; // Admin API Key
-$pltc_key = 'ptlc_'; // Client API Key
+$plta_key = 'ptla_FLJ6Zy59ul0mokSrtMk4LHYpzVEYxNeqPt7sSWGqLfA'; // Admin API Key
+$pltc_key = 'ptlc_dZsj52FRQmPoEnq7P45JiJiNGfOfaxHbyQ7rLy2r9jt'; // Client API Key
 ?>
